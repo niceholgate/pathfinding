03e9e3860be831ada24af2d065b08e0f9a91b24c
Nicholas Holgate <niceholgate@gmail.com>
2022-09-04 19:55:46 +0800

    Improved the abstraction of places. Next I would like to see if Places and Nodes should actually be the same thing. It is awkward to specify ExplicitNeighbours with no distances, and then again with distances in the Node implementation. Would need to reconcile that each set of neighbours matches?

--- csharp_pathfinding/AStarTests/AStarTests.cs
+++ csharp_pathfinding/AStarTests/AStarTests.cs
@@ -4,22 +4,19 @@ using AStarNickNS;
 using System.Collections.Generic;
 using System.Linq;
 
-namespace AStarTests
-{
+namespace AStarTests {
 
     [TestClass]
-    public class GenericPlaceTests
-    {
-        GenericPlace gpA = new GenericPlace("A", new HashSet<IPlace<string>> { });
-        GenericPlace gpB = new GenericPlace("B", new HashSet<IPlace<string>> { });
-        GenericPlace gpC = new GenericPlace("C", new HashSet<IPlace<string>> { });
+    public class GenericPlaceTests {
+        GenericPlace gpA = new GenericPlace("A", new HashSet<IPlace> { });
+        GenericPlace gpB = new GenericPlace("B", new HashSet<IPlace> { });
+        GenericPlace gpC = new GenericPlace("C", new HashSet<IPlace> { });
 
         [TestMethod]
-        public void TestGenericPlace_IsNeighbour()
-        {
+        public void TestGenericPlace_IsNeighbour() {
             // A and B are neighbours, C is disjoint
-            gpA.Neighbours.Add(gpB);
-            gpB.Neighbours.Add(gpA);
+            gpA.ExplicitNeighbours.Add(gpB);
+            gpB.ExplicitNeighbours.Add(gpA);
 
             Assert.IsTrue(gpA.IsNeighbour(gpB));
             Assert.IsTrue(gpB.IsNeighbour(gpA));
@@ -28,86 +25,62 @@ namespace AStarTests
             Assert.IsFalse(gpA.IsNeighbour(gpC));
             Assert.IsFalse(gpB.IsNeighbour(gpC));
 
-            Assert.AreEqual(0, gpC.Neighbours.Count);
-            Assert.AreEqual(1, gpB.Neighbours.Count);
+            Assert.AreEqual(0, gpC.ExplicitNeighbours.Count);
+            Assert.AreEqual(1, gpB.ExplicitNeighbours.Count);
 
             // Check you cannot add a neighbour multiple times to the Neighbours list (HashSet)
-            gpA.Neighbours.Add(gpB);
-            gpA.Neighbours.Add(gpB);
-            gpA.Neighbours.Add(gpB);
-            Assert.AreEqual(1, gpA.Neighbours.Count);
+            gpA.ExplicitNeighbours.Add(gpB);
+            gpA.ExplicitNeighbours.Add(gpB);
+            gpA.ExplicitNeighbours.Add(gpB);
+            Assert.AreEqual(1, gpA.ExplicitNeighbours.Count);
 
             // Check you can remove a neighbour in the HashSet
-            gpA.Neighbours.Remove(gpB);
-            Assert.AreEqual(0, gpA.Neighbours.Count);
+            gpA.ExplicitNeighbours.Remove(gpB);
+            Assert.AreEqual(0, gpA.ExplicitNeighbours.Count);
 
             // Check you cannot remove a non-existent neighbour
-            gpC.Neighbours.Remove(gpB);
-            Assert.AreEqual(0, gpC.Neighbours.Count);
+            gpC.ExplicitNeighbours.Remove(gpB);
+            Assert.AreEqual(0, gpC.ExplicitNeighbours.Count);
         }
 
         [TestMethod]
-        public void TestGenericPlace_ToString()
-        {
+        public void TestGenericPlace_ToString() {
             // Check ToString() method returns the label
             Assert.AreEqual(gpA.Label, "A");
             Assert.AreEqual(gpA.Label, gpA.ToString());
         }
-
-
     }
 
     [TestClass]
-    public class GridCoords2DTests
-    {
+    public class GridCoords2DTests {
         //Assert.ThrowsException<System.ArgumentOutOfRangeException>(() => account.Debit(debitAmount));
-        GridCoords2D gridCoords2DBase = new GridCoords2D(-3, 5);
-        GridCoords2D gridCoords2DDiag = new GridCoords2D(-4, 6);
-        GridCoords2D gridCoords2DStra = new GridCoords2D(-3, 6);
-        GridCoords2D gridCoords2DDistant = new GridCoords2D(-60, 60);
+        GridCoords2D gridCoords2DBase = new GridCoords2D((-3, 5));
+        GridCoords2D gridCoords2DDiag = new GridCoords2D((-4, 6));
+        GridCoords2D gridCoords2DStra = new GridCoords2D((-3, 6));
+        GridCoords2D gridCoords2DDistant = new GridCoords2D((-60, 60));
+        GenericPlace genericPlace = new GenericPlace("GenericPlace");
+        
 
         [TestMethod]
-        public void TestGridCoords2D_GetLabel()
-        {
-            Assert.IsTrue(gridCoords2DBase.Label.Equals((-3, 5)));
+        public void TestGridCoords2D_IsNeighbour() {
+            // Explicit neighbours with any IPlace (not automatically bidirectional)
+            gridCoords2DDistant.ExplicitNeighbours.Add(genericPlace);
+            Assert.IsTrue(gridCoords2DDistant.IsNeighbour(genericPlace));
+            Assert.IsFalse(genericPlace.IsNeighbour(gridCoords2DDistant));
+            gridCoords2DDistant.ExplicitNeighbours.Add(genericPlace);
+            Assert.IsTrue(gridCoords2DDistant.IsNeighbour(genericPlace));
+
+            // Grid neighbours
+            Assert.IsFalse(gridCoords2DBase.IsNeighbour(gridCoords2DDistant));
+
+            Assert.IsTrue(gridCoords2DBase.IsNeighbour(gridCoords2DDiag));
+            Assert.IsTrue(gridCoords2DBase.IsNeighbour(gridCoords2DStra));
+            Assert.IsTrue(gridCoords2DDiag.IsNeighbour(gridCoords2DBase));
+            Assert.IsTrue(gridCoords2DStra.IsNeighbour(gridCoords2DBase));
         }
 
         [TestMethod]
-        public void TestGridCoords2D_IsDiagonalNeighbour()
-        {
-            Assert.AreEqual(true, gridCoords2DBase.IsDiagonalNeighbour(gridCoords2DDiag));
-            Assert.AreEqual(false, gridCoords2DBase.IsDiagonalNeighbour(gridCoords2DStra));
-            Assert.AreEqual(true, gridCoords2DDiag.IsDiagonalNeighbour(gridCoords2DBase));
-            Assert.AreEqual(false, gridCoords2DStra.IsDiagonalNeighbour(gridCoords2DBase));
-
-            Assert.AreEqual(false, gridCoords2DBase.IsDiagonalNeighbour(gridCoords2DDistant));
-        }
-
-        [TestMethod]
-        public void TestGridCoords2D_IsStraightNeighbour()
-        {
-            Assert.AreEqual(false, gridCoords2DBase.IsStraightNeighbour(gridCoords2DDiag));
-            Assert.AreEqual(true, gridCoords2DBase.IsStraightNeighbour(gridCoords2DStra));
-            Assert.AreEqual(false, gridCoords2DDiag.IsStraightNeighbour(gridCoords2DBase));
-            Assert.AreEqual(true, gridCoords2DStra.IsStraightNeighbour(gridCoords2DBase));
-            
-            Assert.AreEqual(false, gridCoords2DBase.IsStraightNeighbour(gridCoords2DDistant));
-        }
-
-        [TestMethod]
-        public void TestGridCoords2D_IsNeighbour()
-        {
-            Assert.AreEqual(true, gridCoords2DBase.IsNeighbour(gridCoords2DDiag));
-            Assert.AreEqual(true, gridCoords2DBase.IsNeighbour(gridCoords2DStra));
-            Assert.AreEqual(true, gridCoords2DDiag.IsNeighbour(gridCoords2DBase));
-            Assert.AreEqual(true, gridCoords2DStra.IsNeighbour(gridCoords2DBase));
-            
-            Assert.AreEqual(false, gridCoords2DBase.IsNeighbour(gridCoords2DDistant));
-        }
-
-        [TestMethod]
-        public void TestGridCoords2D_DeltaFrom()
-        {
+        public void TestGridCoords2D_DeltaFrom() {
             int[] expectedDelta = new int[2] { (-60)-(-3), 60-5 };
             Assert.IsTrue(gridCoords2DDistant.DeltaFrom(gridCoords2DBase).SequenceEqual(expectedDelta));
         }
@@ -118,43 +91,68 @@ namespace AStarTests
         }
     }
 
-    [TestClass]
-    public class NodeTests
-    {
-        static GenericPlace gpA = new GenericPlace("A", new HashSet<IPlace<string>> { });
-        static GenericPlace gpB = new GenericPlace("B", new HashSet<IPlace<string>> { });
-        static GenericPlace gpC = new GenericPlace("C", new HashSet<IPlace<string>> { });
+    //[TestClass]
+    //public class NodeTests {
+    //    static GenericPlace placeA = new GenericPlace("A", new HashSet<IPlace<string>> { });
+    //    static GenericPlace placeB = new GenericPlace("B", new HashSet<IPlace<string>> { });
+    //    static GridCoords2D placeC = new GridCoords2D(-3, 2);
 
-        Node<GenericPlace> nodeA = new Node<GenericPlace>(gpA);
-        Node<GenericPlace> nodeB = new Node<GenericPlace>(gpB);
-        Node<GenericPlace> nodeC = new Node<GenericPlace>(gpC);
+    //    Node<GenericPlace> nodeA = new Node<GenericPlace>(placeA);
+    //    Node<GenericPlace> nodeB = new Node<GenericPlace>(placeB);
+    //    Node<GridCoords2D> nodeC = new Node<GridCoords2D>(placeC);
 
-        public void SetupNodes()
-        {
+    //    public void SetupNodes() {
+    //        Dictionary<Node<GenericPlace>, double> costDictA = new Dictionary<Node<GenericPlace>, double> { { nodeB, 4.0 } };
+    //        foreach (Node<GenericPlace> node in costDictA.Keys) {
+    //            nodeA.Coord.Neighbours.Add(node.Coord);
+    //        }
 
-            Dictionary<Node<GenericPlace>, double> costDictA = new Dictionary<Node<GenericPlace>, double> { { nodeB, 4.0 } };
-            foreach (Node<GenericPlace> node in costDictA.Keys)
-            {
-                nodeA.Coord.Neighbours.Add(node.Coord);
-            }
+    //        Dictionary<Node<GenericPlace>, double> costDictB = new Dictionary<Node<GenericPlace>, double> { { nodeA, 4.0 } };
+    //        foreach (Node<GenericPlace> node in costDictB.Keys) {
+    //            nodeB.Coord.Neighbours.Add(node.Coord);
+    //        }
+    //    }
 
-            Dictionary<Node<GenericPlace>, double> costDictB = new Dictionary<Node<GenericPlace>, double> { { nodeA, 4.0 } };
-            foreach (Node<GenericPlace> node in costDictB.Keys)
-            {
-                nodeB.Coord.Neighbours.Add(node.Coord);
-            }
+    //    [TestMethod]
+    //    public void TestNode_GetLabel() {
+    //        SetupNodes();
+    //        Console.WriteLine(gpA.Neighbours);
+    //        int a = 2;
+    //        //Assert.IsTrue(gridCoords2DBase.GetLabel().SequenceEqual(new int[2] { -3, 5 }));
+    //    }
 
-        }
+    //}
 
-        [TestMethod]
-        public void TestNode_GetLabel()
-        {
-            SetupNodes();
-            Console.WriteLine(gpA.Neighbours);
-            int a = 2;
-            //Assert.IsTrue(gridCoords2DBase.GetLabel().SequenceEqual(new int[2] { -3, 5 }));
-        }
+    // test Node<GenericPlace> ? 
+    //[TestClass]
+    //public class NodeTests {
+    //    static GenericPlace placeA = new GenericPlace("A", new HashSet<IPlace<string>> { });
+    //    static GenericPlace placeB = new GenericPlace("B", new HashSet<IPlace<string>> { });
+    //    static GridCoords2D placeC = new GridCoords2D(-3, 2);
 
+    //    Node<GenericPlace> nodeA = new Node<GenericPlace>(placeA);
+    //    Node<GenericPlace> nodeB = new Node<GenericPlace>(placeB);
+    //    Node<GridCoords2D> nodeC = new Node<GridCoords2D>(placeC);
 
-    }
+    //    public void SetupNodes() {
+    //        Dictionary<Node<GenericPlace>, double> costDictA = new Dictionary<Node<GenericPlace>, double> { { nodeB, 4.0 } };
+    //        foreach (Node<GenericPlace> node in costDictA.Keys) {
+    //            nodeA.Coord.Neighbours.Add(node.Coord);
+    //        }
+
+    //        Dictionary<Node<GenericPlace>, double> costDictB = new Dictionary<Node<GenericPlace>, double> { { nodeA, 4.0 } };
+    //        foreach (Node<GenericPlace> node in costDictB.Keys) {
+    //            nodeB.Coord.Neighbours.Add(node.Coord);
+    //        }
+    //    }
+
+    //    [TestMethod]
+    //    public void TestNode_GetLabel() {
+    //        SetupNodes();
+    //        Console.WriteLine(gpA.Neighbours);
+    //        int a = 2;
+    //        //Assert.IsTrue(gridCoords2DBase.GetLabel().SequenceEqual(new int[2] { -3, 5 }));
+    //    }
+
+    //}
 }
--- csharp_pathfinding/A_star/AStarClasses.cs
+++ csharp_pathfinding/A_star/AStarClasses.cs
@@ -15,52 +15,66 @@ namespace AStarNickNS
     //}
 
     //////////// INTERFACES
-
+    /*
+     * A place which can check if any other place is a neighbour
+     */
     public interface IPlace {
-
+        bool IsNeighbour(IPlace other);
     }
 
     /*
-     * A place
+     * A place with a descriptive type e.g. a string name, or a grid coordinate
      */
-    public interface IPlace<LabelType> : IPlace {
+    public interface IPlace<out LabelType> : IPlace {
         LabelType Label { get; }
-
-        bool IsNeighbour(IPlace<LabelType> other);
     }
 
+    /*
+     * A node which can find the cost of getting to any neighbour (even if the neighbour has a different place type)
+     */
     public interface INode {
         double GetCostToLeave(INode neighbour);
     }
 
     /*
-     * A node
+     * A node with a particular place type
      */
     public interface INode<CoordType> : INode where CoordType : IPlace {
         CoordType Coord { get; }
     }
 
     /*
-     * An AStar node
+     * An AStar node - has heuristic distance calculation ability for its place type
      */
     public interface INodeAStar<CoordType> : INode<CoordType> where CoordType : IPlace {
         double GetHeuristicDist(INodeAStar<CoordType> other, Distances2D.HeuristicType heuristicType);
     }
 
+    public abstract class Place : IPlace {
+        public HashSet<IPlace> ExplicitNeighbours { get; }
+
+        protected Place(HashSet<IPlace> explicitNeighbours) {
+            ExplicitNeighbours = explicitNeighbours;
+        }
+
+        public abstract bool IsNeighbour(IPlace other);
+
+        protected bool IsNeighbourExplicit(IPlace other) { return ExplicitNeighbours.Contains(other); }
+    }
+
     /*
      * A place with no defining geometry, just explicitly specified neighbours
      */
-    public struct GenericPlace : IPlace<string> {
-        public GenericPlace(string label, HashSet<IPlace<string>> neighbours) {
-            Label = label;
-            Neighbours = neighbours;
-        }
-
+    public class GenericPlace : Place, IPlace<string> {
         public string Label { get; }
 
-        public bool IsNeighbour(IPlace<string> other) { return Neighbours.Contains(other); }
+        public GenericPlace(string label) : this(label, new HashSet<IPlace>()) { }
 
-        public HashSet<IPlace<string>> Neighbours { get; }
+        public GenericPlace(string label, HashSet<IPlace> explicitNeighbours) : base(explicitNeighbours) {
+            Label = label;
+        }
+
+        public override bool IsNeighbour(IPlace other) { return IsNeighbourExplicit(other); }
 
         public override string ToString() => Label;
     }
@@ -68,57 +82,72 @@ namespace AStarNickNS
     /*
      * A place on a 2D grid
      */
-    public struct GridCoords2D : IPlace<(int, int)> {
-        public GridCoords2D(int x, int y) { Label = (x, y); }
+    public class GridCoords2D : Place, IPlace<(int, int)> {
+        public GridCoords2D((int, int) label) : this(label, new HashSet<IPlace>()) { }
+
+        public GridCoords2D((int, int) label, HashSet<IPlace> explicitNeighbours) : base(explicitNeighbours) {
+            Label = label;
+        }
 
         public (int, int) Label { get; }
 
-        public bool IsDiagonalNeighbour(IPlace<(int, int)> other) {
+        public override bool IsNeighbour(IPlace other) {
+            if (IsNeighbourExplicit(other)) { return true; }
+            if (other is GridCoords2D) { return IsNeighbourGrid((GridCoords2D)other); }
+            return false;
+        }
+
+        private bool IsNeighbourExplicit(IPlace other) {
+            return ExplicitNeighbours.Contains(other);
+        }
+
+        private bool IsNeighbourGrid(GridCoords2D other) { return IsDiagonalNeighbour(other) || IsStraightNeighbour(other); }
+
+        private bool IsDiagonalNeighbour(GridCoords2D other) {
             int[] delta = DeltaFrom(other);
             return delta[0] * delta[0] + delta[1] * delta[1] == 2;
         }
 
-        public bool IsStraightNeighbour(IPlace<(int, int)> other) {
+        private bool IsStraightNeighbour(GridCoords2D other) {
             int[] delta = DeltaFrom(other);
             return Math.Abs(delta[0]) + Math.Abs(delta[1]) == 1;
         }
 
-        public bool IsNeighbour(IPlace<(int, int)> other) { return IsDiagonalNeighbour(other) || IsStraightNeighbour(other); }
-
-        public int[] DeltaFrom(IPlace<(int, int)> other) { return new int[] { this.Label.Item1 - other.Label.Item1, this.Label.Item2 - other.Label.Item2 }; }
+        public int[] DeltaFrom(GridCoords2D other) { return new int[] { this.Label.Item1 - other.Label.Item1, this.Label.Item2 - other.Label.Item2 }; }
 
         public override string ToString() => Label.ToString();
     }
 
-    abstract class Node<CoordType> : INode<CoordType> where CoordType : IPlace {
-        public Node(CoordType coord) { this.Coord = coord; }
+    //abstract class Node<CoordType> : INode<CoordType> where CoordType : IPlace {
+    //    public Node(CoordType coord) { this.Coord = coord; }
 
-        public CoordType Coord { get; }
+    //    public CoordType Coord { get; }
 
-        public IDictionary<INode<CoordType>, double> GetNeighboursCosts() { return this.neighboursCosts; }
+    //    public IDictionary<INode<CoordType>, double> GetNeighboursCosts() { return this.neighboursCosts; }
 
-        public virtual double GetCostToLeave(INode neighbour) { return neighboursCosts[neighbour]; }
-    }
+    //    public virtual double GetCostToLeave(INode neighbour) { return neighboursCosts[neighbour]; }
+    //}
 
-    public class Square : Node<GridCoords2D>, INodeAStar<GridCoords2D> {
-        public static readonly double SQRT2 = Math.Sqrt(2.0);
+    //public class Square : Node<GridCoords2D>, INodeAStar<GridCoords2D> {
+    //    public static readonly double SQRT2 = Math.Sqrt(2.0);
 
-        public Square(GridCoords2D coord) : base(coord) { }
+    //    public Square(GridCoords2D coord) : base(coord) { }
 
-        public double GetCostToLeave(Square neighbour) {
-            double distance = neighbour.IsDiagonalNeighbour(this) ? SQRT2 : 1.0;
-            return neighboursCosts[neighbour] * distance;
-        }
+    //    public double GetCostToLeave(Square neighbour) {
+    //        double distance = neighbour.IsDiagonalNeighbour(this) ? SQRT2 : 1.0;
+    //        return neighboursCosts[neighbour] * distance;
+    //    }
 
-        private bool IsDiagonalNeighbour(Square other) { return Coord.IsDiagonalNeighbour(other.Coord); }
+    //    private bool IsDiagonalNeighbour(Square other) { return Coord.IsDiagonalNeighbour(other.Coord); }
 
-        public double GetHeuristicDist(INodeAStar<GridCoords2D> other, Distances2D.HeuristicType heuristicType) {
-            double[] thisLabelAsDoubles = { Coord.Label.Item1 , Coord.Label.Item2 };
-            double[] otherLabelAsDoubles = { other.Coord.Label.Item1, other.Coord.Label.Item2 };
-            return Distances2D.GetDistance(thisLabelAsDoubles, otherLabelAsDoubles, heuristicType);
-        }
+    //    public double GetHeuristicDist(INodeAStar<GridCoords2D> other, Distances2D.HeuristicType heuristicType) {
+    //        double[] thisLabelAsDoubles = { Coord.Label.Item1 , Coord.Label.Item2 };
+    //        double[] otherLabelAsDoubles = { other.Coord.Label.Item1, other.Coord.Label.Item2 };
+    //        return Distances2D.GetDistance(thisLabelAsDoubles, otherLabelAsDoubles, heuristicType);
+    //    }
+    //}
 
-    }
+
     //class Hex : Node<HexCoords2D>, INodeAStar<HexCoords2D>
     //{
     //    interface_method(Hex hex)
